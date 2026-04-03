ac3ee43c5a5e4f185e239657256fe21ebf74cffc
kdennewith <kdennewith@gmail.com>
2026-04-02 20:06:19 -0700

    Actor & Components (Projectile Attack) Video Committing. Added Textures to the Project Directory and tweaked some Collision stuff on the SphereComponent.

---
+++ Source/ActionRL/Projectiles/RLProjectileMagic.cpp
@@ -0,0 +1,28 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "RLProjectileMagic.h"
+
+#include "Components/SphereComponent.h"
+#include "GameFramework/ProjectileMovementComponent.h"
+#include "NiagaraComponent.h"
+
+// Sets default values
+ARLProjectileMagic::ARLProjectileMagic()
+{
+	
+	SphereComponent = CreateDefaultSubobject<USphereComponent>(TEXT("SphereComp"));
+	RootComponent = SphereComponent;
+	SphereComponent->SetSphereRadius(16.f);
+	SphereComponent->SetCollisionProfileName(TEXT("Projectile"));
+	
+	LoopedNiagaraComponent = CreateDefaultSubobject<UNiagaraComponent>(TEXT("LoopedNiagara"));
+	LoopedNiagaraComponent->SetupAttachment(SphereComponent);
+	
+	ProjectileMovementComponent = CreateDefaultSubobject<UProjectileMovementComponent>(TEXT("ProjectileMoveComp"));
+	ProjectileMovementComponent->InitialSpeed = 2000.f;
+	ProjectileMovementComponent->ProjectileGravityScale = 0.f; /* So the Projectile doesn't fall through the floor */
+	
+}
+
+
---
+++ Source/ActionRL/Projectiles/RLProjectileMagic.h
@@ -0,0 +1,31 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "RLProjectileMagic.generated.h"
+
+class USphereComponent;
+class UProjectileMovementComponent;
+class UNiagaraComponent;
+
+UCLASS()
+class ACTIONRL_API ARLProjectileMagic : public AActor
+{
+	GENERATED_BODY()
+
+protected:
+	
+	UPROPERTY(EditDefaultsOnly, Category="Components")
+	TObjectPtr<USphereComponent> SphereComponent;
+	
+	UPROPERTY(EditDefaultsOnly, Category="Components")
+	TObjectPtr<UNiagaraComponent> LoopedNiagaraComponent;
+	
+	UPROPERTY(EditDefaultsOnly, Category="Components")
+	TObjectPtr<UProjectileMovementComponent> ProjectileMovementComponent;
+	
+public:
+	ARLProjectileMagic();
+};
