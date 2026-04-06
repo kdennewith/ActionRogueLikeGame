d7e52897e54d8d1f26c0838bd90509474b175320
kdennewith <kdennewith@gmail.com>
2026-04-06 16:20:24 -0700

    Interface commit

---
+++ Source/ActionRL/Core/RLInteractionInterface.h
@@ -0,0 +1,27 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UObject/Interface.h"
+#include "RLInteractionInterface.generated.h"
+
+// This class does not need to be modified.
+UINTERFACE()
+class URLInteractionInterface : public UInterface
+{
+	GENERATED_BODY()
+};
+
+/**
+ * 
+ */
+class ACTIONRL_API IRLInteractionInterface
+{
+	GENERATED_BODY()
+
+	// Add interface functions to this class. This is the class that will be inherited to implement this interface.
+public:
+	
+	virtual void Interact() = 0;  /** Pure Virtual Function */
+};
